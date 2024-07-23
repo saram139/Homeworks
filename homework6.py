a43e2c7902423eacba206fb6833692b154c948a6
Михаил <saram139@mail.ru>
2024-07-23 21:16:00 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,14 @@
+my_dict = {'Антон': 1996, 'Егор': 2000, 'Роман': 1992}
+print('Dict:', my_dict)
+print('Existing value:', my_dict['Антон'])
+print('Not existing value:', my_dict.get('Елена'))
+my_dict.update({'Елена': 2001, 'Ольга': 1990})
+print('Deleted value:', my_dict.pop('Егор'))
+print('Modified dictionary:', my_dict, '\n')
+
+my_set = {1, 2, 1, 3, (1, 2), '7', '7'}
+print('Set:', my_set)
+my_set.add('Банан')
+my_set.add(7)
+my_set.discard(3)
+print('Modified set:', my_set)
